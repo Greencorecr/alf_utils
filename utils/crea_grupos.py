2612edfd63542b17046f56faece422a40bc4aded
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-03-04 12:35:20 -0600

    Agregando líneas para nombres cortos

--- utils/crea_grupos.py
+++ utils/crea_grupos.py
@@ -7,7 +7,6 @@ import json
 import re
 import requests
 
-
 URL = 'http://localhost:8080/alfresco/api/-default-/public/alfresco/versions/1/groups'
 
 SUBGRUPOS = ["Recepción", "Elaboración", "Gestionar", "Expediente", "Firmador", "Archivar"]
@@ -31,13 +30,16 @@ for group in groups['list']['entries']:
     if len(result[0]) == 3 and result[len(result)-1] not in EXCEPCIONES: # ABC- in pattern
         if len(result)==2:  # ABC-DEF-palabra
             for newgrp in SUBGRUPOS:
-                print("Creando grupo llamado: " + result[0] + "-" + result[1] + "-" + newgrp)
+                string=result[0] + "-" + result[1] + '-' + newgrp
+                print("Creando grupo llamado: ", string)
+                post_data='{"id": "' + string + '", "displayName": "' + string + '"}'
+                #response = requests.post(URL, data=post_data, headers=headers)
+                #print(response.text)
         else:
             if len(result)==3: # ABC-DEF-HIG-Palabra
                 for newgrp in SUBGRUPOS:
                     string=result[0] + "-" + result[1] + '-' + result[2] + "-" + newgrp
                     print("Creando grupo llamado: ", string)
                     post_data='{"id": "' + string + '", "displayName": "' + string + '"}'
-                    print(post_data)
                     #response = requests.post(URL, data=post_data, headers=headers)
                     #print(response.text)
