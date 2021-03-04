c7f91ee926bd2df01289777815e3cf469d5f61b4
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-03-04 09:06:39 -0600

    Agregando código para nombres de grupos a crear

--- utils/crea_grupos.py
+++ utils/crea_grupos.py
@@ -5,8 +5,11 @@ Utilitario para crear grupos dependiendo de ciertos parámetros
 import base64
 import json
 import requests
+import re
 
 URL = 'http://172.20.1.4:8080/alfresco/api/-default-/public/alfresco/versions/1/groups'
+
+SUBGRUPOS = ["Recepción", "Elaboración", "Gestionar", "Expediente", "Firmador", "Archivar"]
 
 USER = 'admin'
 PASSWD = 'admin'
@@ -19,4 +22,14 @@ groups = json.loads(response.text)
 #{'entry': {'isRoot': True, 'displayName': 'SITE_ADMINISTRATORS',
 # 'id': 'GROUP_SITE_ADMINISTRATORS'}}
 for group in groups['list']['entries']:
-    print(group['entry']['displayName'])
+    #print(group['entry']['displayName'])
+    new_string = re.sub('\n', '', group['entry']['displayName'])
+    result = re.split('-', new_string)
+    if len(result[0]) == 3:
+        if len(result)==2:
+            for newgrp in SUBGRUPOS:
+                print("Creando grupo llamado: " + result[0] + "-" + result[1] + "-" + newgrp)
+        else:
+            if len(result)==3:
+                for newgrp in SUBGRUPOS:
+                    print("Creando grupo llamado: " + result[0] + "-" + result[1] + '-' + result[2] + "-" + newgrp)
