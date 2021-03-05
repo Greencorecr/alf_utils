4e3e3687c72a6494f34c814d54c3925d5f09d79e
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-03-05 15:31:12 -0600

    It's not necesary to fix all perms, only alf_data

--- fixperms/fixperms.sh
+++ fixperms/fixperms.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
 
-cd /tmp  
-wget https://github.com/Greencorecr/alf_utils/raw/main/fixperms/template.tar.xz
-tar xJf template.tar.xz
-cd /opt/alfresco
-find . -print -exec chown --reference=/tmp/alfresco-perms/{} {} \; 2> /dev/null
+#cd /tmp  
+#wget https://github.com/Greencorecr/alf_utils/raw/main/fixperms/template.tar.xz
+#tar xJf template.tar.xz
+#cd /opt/alfresco
+#find . -print -exec chown --reference=/tmp/alfresco-perms/{} {} \; 2> /dev/null
 chown -R alfresco:alfresco /opt/alfresco/alf_data
 chown -R alfresco:alfresco /opt/alfresco/logs/
 
