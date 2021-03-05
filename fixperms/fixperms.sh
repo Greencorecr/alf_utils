3d3ea5bcc6cff12ecae9c18a64e7eb8279064981
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-03-05 13:49:36 -0600

    Less files on template, chown for alf_data

--- fixperms/fixperms.sh
+++ fixperms/fixperms.sh
@@ -2,6 +2,7 @@
 
 cd /tmp  
 wget https://github.com/Greencorecr/alf_utils/raw/main/fixperms/template.tar.xz
-tar xJvf template.tar.xz
+tar xJf template.tar.xz
 cd /opt/alfresco
 find . -print -exec chown --reference=/tmp/alfresco-perms/{} {} \; 2> /dev/null
+chown -R alfresco:alfresco /opt/alfresco/alf_data
