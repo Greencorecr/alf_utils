2f00cad8219793cc8728e0f6b642248617b4294c
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-03-12 12:59:14 -0600

    Re-adding template for alfresco/

--- fixperms/fixperms.sh
+++ fixperms/fixperms.sh
@@ -1,5 +1,11 @@
 #!/bin/env bash
 
+cd /tmp  
+wget https://github.com/Greencorecr/alf_utils/raw/main/fixperms/template.tar.xz
+tar xJf template.tar.xz
+cd /opt/alfresco
+find . -print -exec chown --reference=/tmp/alfresco-perms/{} {} \; 2> /dev/null
+
 chown -R alfresco:alfresco /opt/alfresco/alf_data
 chown -R alfresco:alfresco /opt/alfresco/logs/
 chown -R alfresco:alfresco /opt/alfresco/logs/solr6
