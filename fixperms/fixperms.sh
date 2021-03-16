525c050a84b6efd624966fcb2c011c26727def1e
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-03-16 10:34:07 -0600

    Adding wkhtmltopdf+link

--- fixperms/fixperms.sh
+++ fixperms/fixperms.sh
@@ -1,5 +1,10 @@
 #!/bin/env bash
 
+apt-get -qy install wkhtmltopdf
+if [ -L /usr/local/bin/wkhtmltopdf ]; then
+  ln -s /usr/bin/wkhtmltopdf /usr/local/bin/
+fi
+
 cd /tmp  
 wget https://github.com/Greencorecr/alf_utils/raw/main/fixperms/template.tar.xz
 tar xJf template.tar.xz
@@ -7,6 +12,7 @@ cd /opt/alfresco
 find . -print -exec chown --reference=/tmp/alfresco-perms/{} {} \; 2> /dev/null
 find . -print -exec chmod --reference=/tmp/alfresco-perms/{} {} \; 2> /dev/null
 find /opt/alfresco -uid 109 -exec echo chown alfresco:alfresco {} \;
+rm -r /tmp/alfresco-perms /tmp/template.tar.xz
 
 chown -R alfresco:alfresco /opt/alfresco/alf_data
 chown -R alfresco:alfresco /opt/alfresco/logs/
