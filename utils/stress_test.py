9491cf5bf1a4ecf55994b5fda51cf5e6615a513b
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-03-22 11:17:33 -0600

    Passing pylint

--- utils/stress_test.py
+++ utils/stress_test.py
@@ -7,9 +7,11 @@ import os
 import json
 import random
 
-HOST = 'localhost'
+HOST = '10.42.25.237'
+#HOST = 'localhost'
 NODE_URL = 'nodes/-root-/children'
-URL = 'http://' + HOST + ':8080/alfresco/api/-default-/public/alfresco/versions/1/' + NODE_URL
+URL_API = '/alfresco/api/-default-/public/alfresco/versions/1/'
+URL = 'http://' + HOST + ':8080' + URL_API + NODE_URL
 CURL_OPTS1 = ' -X POST -F filedata=@./testfile -F "nodeType=cm:content" -F "cm:title=My text" '
 CURL_OPTS2 = ' -F "cm:description=My text document description" -F "relativePath=My Folder" '
 AUTH = ' -H "Authorization: Basic YWRtaW46YWRtaW4=" '
@@ -17,7 +19,7 @@ AUTH = ' -H "Authorization: Basic YWRtaW46YWRtaW4=" '
 print("Prueba de subida. Subiendo un archivo de 50MiB, 25 veces")
 test_start_time = time.time()
 
-for _ in range(1, 25):
+for _ in range(1, 2):
     start_time = time.time()
     RAND = str(random.randint(0, 10000))
     CURL_CMD = 'curl -s -F "name=somefile' + RAND +'.txt" ' + CURL_OPTS1 + CURL_OPTS2 + AUTH + URL
@@ -34,7 +36,7 @@ last_file = json.loads(result)
 
 
 CURL_OPTS1 = 'curl -s -X GET --output /dev/null '
-URL = 'http://' + HOST + ':8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/' + last_file['entry']['id'] + '/content'
+URL = 'http://' + HOST + ':8080' + URL_API + '/nodes/' + last_file['entry']['id'] + '/content'
 print("Prueba de bajada. Descargando el archivo recién subido, de 50MiB, 25 veces")
 test_start_time = time.time()
 
@@ -42,6 +44,7 @@ for _ in range(1, 25):
     start_time = time.time()
     RAND = str(random.randint(0, 10000))
     CURL_CMD = CURL_OPTS1 + AUTH + URL
+    print(CURL_CMD)
     result = os.popen(CURL_CMD).read()
     test_time = time.time() - start_time
     if test_time > 5:
@@ -50,5 +53,3 @@ for _ in range(1, 25):
         print("OK")
 test_test_time = time.time() - test_start_time
 print("--- Test %s seconds ---" % test_test_time)
-
-
